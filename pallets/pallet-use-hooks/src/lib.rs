8464b0ee9bd9f824ce9a30c51a8a2ff04893d689
凤之兮原 <1577310596@qq.com>
2022-11-11 20:38:34 +0800

    feat: code use-hooks Event

--- pallets/pallet-use-hooks/src/lib.rs
+++ pallets/pallet-use-hooks/src/lib.rs
@@ -1,123 +1,78 @@
-// allow the program to compile in native rust binary
-// and also Wasm, all should have the no-std attribute
 #![cfg_attr(not(feature = "std"), no_std)]
 
-// import the pallet library so as to be able
-// to use the related methods from the crate
 pub use pallet::*;
 
-// start the pallet modules and import the
-// frame support macros for help
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
-	// define the pallet class to hold
-	// an underlying struct for our implementation
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	// define the config trait which every pallet
-	// needs to define the parameters & types in its function
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		// define the events that would be captured
-		// for each function and parameter type
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 	}
 
-	// define the storage runtime pallet macro
-	// this should hold what we want to store
 	#[pallet::storage]
 	#[pallet::getter(fn something)]
-	pub(super) type Claims<T: Config> = StorageMap<
-		_,
-		Blake2_128Concat,
-		T::Hash,
-		(T::AccountId, T::BlockNumber)
-	>;
+	pub type Param<T: Config> = StorageValue<_, u32, ValueQuery>;
 
-	// define the events dispatcher macro, this should
-	// throw certain events based on the action taken
+	#[pallet::storage]
+	pub type SetFlag<T: Config> = StorageValue<_, bool, ValueQuery>;
+
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	// pub enum Event<T: Config> {
+	// 	setParamBiggerThan100 {
+	// 		who: T::AccountId,
+	// 		claim: T::Hash,
+	// 	},
+	// }
 	pub enum Event<T: Config> {
-		ClaimCreated {
-			who: T::AccountId,
-			claim: T::Hash,
-		},
-		ClaimRevoked {
-			who: T::AccountId,
-			claim: T::Hash,
-		},
+		SetParam(u32),
 	}
 
-	// define the macros for the runtime errors
-	// should be an enum for errors
-	#[pallet::error]
-	pub enum Error<T> {
-		AlreadyClaimed,
-		NoSuchClaim,
-		NotClaimOwner,
-	}
-
-	// define the pallet call macro which
-	// would hold the core functions and function
-	// definition of the proof of existence.
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		// users have to pay a certain gas fee
-		// in order to perform an extrinsic(off chain)
-		// activity in our Blockchain
-		#[pallet::weight(0)]
-		pub fn create_claims(origin: OriginFor<T>, claim: T::Hash) -> DispatchResult {
-			// check if the sender of the digital file signature
-			// is a signed user else return false.
-			let sender = ensure_signed(origin)?;
-
-			// also ensure that the hash being passed is not
-			// already stored in our blockchain database
-			ensure!(!Claims::<T>::contains_key(&claim), Error::<T>::AlreadyClaimed);
-
-			// since at this point we have verified that the claim
-			// does not exist [assuming the logic above does not throw an error]
-			// then we insert the new claim into our Blockchain store
-			let current_block = <frame_system::Pallet<T>>::block_number();
-			Claims::<T>::insert(&claim, (&sender, current_block));
-
-			// Dispatch an event telling the system that a claim has been created
-			// this should be recorded as a state of transition in our block
-			Self::deposit_event(Event::ClaimCreated {
-				who: sender,
-				claim,
-			});
-			Ok(())
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_initialize(n: T::BlockNumber) -> Weight {
+			log::info!("on_initialize: {:?}", n);
+			0
 		}
 
+		fn on_finalize(n: T::BlockNumber) {
+			log::info!(target: "use-hooks", "------------ on_finalize, block number is {:?}", n);
+		}
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		ParamInvalid,
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[transactional]
 		#[pallet::weight(0)]
-		pub fn revoke_claims(origin: OriginFor<T>, claim: T::Hash) -> DispatchResult {
-			// ensure that the user to revoke the claim is a valid
-			// and signed user
+		fn set_param_bigger_than_100(orogin: OriginFor<T>, param: u32) -> DispatchResult {
+			// 1. 判断调用者权限
+			ensure_root(orogin)?;
 
-			let sender = ensure_signed(origin)?;
+			// 2. 业务逻辑
+			// 2.1 将标志位设置为 true
+			SetFlag::<T>::put(true);
 
-			// check that the claim which wants to be
-			// revoked actually exists
-			let (owner, _) = Claims::<T>::get(&claim).ok_or(Error::<T>::NoSuchClaim)?;
+			// 2.2. 如果参数 > 100，则写入到 storage param 中
+			if param <= 100i32 {
+				return Err(Error::<T>::ParamInvalid.into());
+			}
 
-			// confirm that the signed user that wants to revoke
-			// the claim is the same user that uploaded the claim
-			ensure!(sender == owner, Error::<T>::NotClaimOwner);
-
-			// then remove the claim from Map
-			Claims::<T>::remove(&claim);
-
-			// Emit an event to show that claim was revoked
-			Self::deposit_event(Event::ClaimRevoked { who: sender, claim });
-
-			Ok(())
+			// 3. 发出事件
+			Self::deposit_event(Event::SetParam(param));
+			log::info!(target: "use-hooks", "set param bigger then 100");
+			Ok(().into())
 		}
 	}
 }
